a818b178471b92fc58d1098d8ba3a7e7f0aeeafa
purighella <139880774+purighella@users.noreply.github.com>
2023-10-05 09:38:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Parents New Healthy Diner - it is a dosa place in Secunderabad
+
+                Breakfast Menu
+                
+                Omega 3: Blueberry dosa
+                Ghee dosa with spinach and onions
+                Chutney and Sambar')
