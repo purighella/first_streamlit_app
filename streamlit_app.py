572e7eccf02c482d45ea9cdf8badbd29863bfc8a
purighella <139880774+purighella@users.noreply.github.com>
2023-10-05 09:59:19 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents\' New Healthy Diner - it is a \'dosa place\' in Secunderabad')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Idli Sambar')
-streamlit.text('Ghee dosa with onions') 
-streamlit.text('Chutney, Potato masala curry and Sambar')
+streamlit.header('🥣Breakfast Menu')
+streamlit.text('🥗 Idli Sambar')
+streamlit.text('🐔 Ghee dosa with onions') 
+streamlit.text('🥑🍞Chutney, Potato masala curry and Sambar')
