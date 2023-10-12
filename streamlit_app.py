0744fb63081cf218fb7147cd4dde80d0cddb2b8c
purighella <139880774+purighella@users.noreply.github.com>
2023-10-12 15:58:40 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,10 +26,10 @@ streamlit.dataframe(fruits_to_show)
 
 
 #create the repeatable code block (called a function)
-#def get_fruityvice_data(this_fruit_choice):
-#      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-#     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#     return fruityvice_normalized
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
   
 #New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
@@ -38,10 +38,8 @@ try:
   if not fruit_choice:
       streamlit.error("Please select a fruit to get information.")
   else:
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-      #back_from_function = get_fryutiyvice_data(fruit_choice)
-      streamlit.dataframe(fruityvice_normalized)
+      back_from_function = get_fryutiyvice_data(fruit_choice)
+      streamlit.dataframe(back_from_function)
     
 except URLError as e:
   streamlit.error()
